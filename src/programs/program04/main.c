1b7a69f90df120fca71dc884a9f2391be19a3820
Carinapremi67 <prem240163@sr.htlweiz.at>
2026-04-23 08:44:08 +0000

    test

--- src/programs/program04/main.c
+++ src/programs/program04/main.c
@@ -4,12 +4,18 @@
 
 #include <inttypes.h>
 #include <stdio.h>
+#include <math.h>
 
 // TODO: your code goes here
-
+double compute_hypotenuse(double cath_a, double cath_b) {
+  double compute_hypotenuse = 0;
+  double compute_hypotenuse = sqrt((cath_a * cath_a) + (cath_b * cath_b));
+  return compute_hypotenuse;
+} 
 int main(void) {
   double leg_a = 0, leg_b = 0;
 
+  printf("Geben sie eine Länge der Kathete a ein: ");
   scanf("%lf", &leg_a);
 
   printf("%f", compute_hypotenuse(leg_a, leg_b));
