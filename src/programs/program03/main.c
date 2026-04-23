795ae5a5cff674d18417c10d32c3a746b89afa57
Carinapremi67 <prem240163@sr.htlweiz.at>
2026-04-23 09:02:31 +0000

    test 3

--- src/programs/program03/main.c
+++ src/programs/program03/main.c
@@ -8,13 +8,14 @@
 // TODO: your code goes here
 double Celsius2Fahrenheit (double temp_celsius){
 double Celsius2Fahrenheit = 0;
-Celsius2Fahrenheit = 20*9/5+32;
+Celsius2Fahrenheit = temp_celsius*9/5+32;
 return Celsius2Fahrenheit;
 
 }
 int main(void) {
   double temp_celsius = 0;
 
+  printf("Geben sie eine Temperatur ein: ");
   scanf("%lf", &temp_celsius);
 
   printf("%f", Celsius2Fahrenheit(temp_celsius));
